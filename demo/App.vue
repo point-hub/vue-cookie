f82a7d526a55905fa87dd56ec793040cbf2c3486
Martien Dermawan Tanama <martiendt@gmail.com>
2021-05-21 13:47:38 +0700

    update demo page

--- demo/App.vue
+++ demo/App.vue
@@ -1,71 +1,103 @@
 <template>
-  <div class="container m-auto">
-    <div class="m-4">
-      <h1 class="my-2 text-xl font-bold">Cookie Demo</h1>
-      <p class="my-2">
-        Please check your browser cookie to confirm
-      </p>
-      <hr class="mb-10" />
-      <h2 class="my-2 text-lg">Add Cookie</h2>
-      <div class="flex">
-        <input
-          type="text"
-          class="px-2 mr-2 border border-gray-600"
-          v-model="form.name"
-          placeholder="key"
-        />
-        <input
-          type="text"
-          class="px-2 mr-2 border border-gray-600"
-          v-model="form.value"
-          placeholder="value"
-        />
-        <button
-          class="px-4 py-1 text-white uppercase bg-blue-700"
-          @click="set(form.name, form.value)"
-        >
-          Save
-        </button>
-      </div>
-      <br />
-      <div>
-        <h2 class="my-2 text-lg">List Cookie</h2>
-        <div class="overflow-auto">
-          <table class="w-full border border-gray-500 table-fixed">
-            <thead class="text-left uppercase">
-              <tr>
-                <th class="p-2">Key</th>
-                <th class="p-2">Value</th>
-                <th class="p-2"></th>
-              </tr>
-            </thead>
-            <tbody class="text-sm">
-              <tr v-for="(cookie, index) in cookies" :key="index">
-                <td class="p-2 break-words border-t border-b border-gray-500">
-                  {{ cookie[0] }}
-                </td>
-                <td class="p-2 break-words border-t border-b border-gray-500">
-                  {{ cookie[1] }}
-                </td>
-                <td
-                  class="p-2 text-right break-words border-t border-b border-gray-500"
-                >
-                  <button
-                    @click="view(cookie[0])"
-                    class="px-4 py-1 text-xs text-white uppercase bg-blue-500 border"
+  <div class="bg-gray-200">
+    <div class="container m-auto bg-white shadow-lg">
+      <div class="h-full p-4">
+        <div class="flex justify-between">
+          <div>
+            <h1 class="my-2 text-3xl font-bold">Cookie Demo</h1>
+            <p class="my-2">
+              Please check your browser cookie to confirm
+            </p>
+          </div>
+          <div class="flex">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="github"
+              class="w-6 h-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 496 512"
+            >
+              <path
+                fill="currentColor"
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+              ></path>
+            </svg>
+            <span class="ml-2">
+              <a href="https://github.com/point-hub/vue-cookie">
+                @point-hub/vue-cookie
+              </a>
+            </span>
+          </div>
+        </div>
+        <hr class="mb-10" />
+        <h2 class="my-2 text-lg">Add Cookie</h2>
+        <div class="flex">
+          <input
+            type="text"
+            class="px-2 mr-2 border border-gray-600"
+            v-model="form.key"
+            placeholder="key"
+          />
+          <input
+            type="text"
+            class="px-2 mr-2 border border-gray-600"
+            v-model="form.value"
+            placeholder="value"
+          />
+          <button
+            class="px-4 py-1 text-white uppercase bg-blue-700"
+            @click="set(form.key, form.value)"
+          >
+            Save
+          </button>
+        </div>
+        <br />
+        <pre><code v-highlight>{{ scAddCookie }}</code></pre>
+        <br />
+        <div>
+          <h2 class="my-2 text-lg">List Cookie</h2>
+          <div class="overflow-auto">
+            <table class="w-full border border-gray-500 table-fixed">
+              <thead class="text-left uppercase">
+                <tr>
+                  <th class="p-2">Key</th>
+                  <th class="p-2">Value</th>
+                  <th class="p-2"></th>
+                </tr>
+              </thead>
+              <tbody class="text-sm">
+                <tr v-for="(cookie, index) in cookies" :key="index">
+                  <td class="p-2 break-words border-t border-b border-gray-500">
+                    {{ cookie[0] }}
+                  </td>
+                  <td class="p-2 break-words border-t border-b border-gray-500">
+                    {{ cookie[1] }}
+                  </td>
+                  <td
+                    class="p-2 text-right break-words border-t border-b border-gray-500"
                   >
-                    View
-                  </button>
-                  <button
-                    @click="remove(cookie[0])"
-                    class="px-4 py-1 text-xs text-white uppercase bg-red-500 border"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                    <button
+                      @click="view(cookie[0])"
+                      class="w-20 py-2 text-xs text-white uppercase bg-blue-500 border"
+                    >
+                      View
+                    </button>
+                    <button
+                      @click="remove(cookie[0])"
+                      class="w-20 py-2 text-xs text-white uppercase bg-red-500 border"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <br />
+          <pre><code v-highlight>{{ scGetCookie }}</code></pre>
         </div>
       </div>
     </div>
@@ -81,33 +113,47 @@ export default {
   data() {
     return {
       form: {
-        name: null,
+        key: null,
         value: null
       },
-      cookies: []
+      cookies: [],
+      scAddCookie: `import cookie from "@point-hub/vue-cookie";
+
+set(key, value) {
+  cookie.set(key, value);
+}`,
+      scGetCookie: `import cookie from "@point-hub/vue-cookie";
+
+view(key) {
+  alert(cookie.get(key));
+}
+
+remove(key) {
+  cookie.remove(key);
+},`
     };
   },
   mounted() {
     this.refreshList();
   },
   methods: {
-    set(name, value) {
-      cookie.set(name, value);
+    set(key, value) {
+      cookie.set(key, value);
       this.clearForm();
       this.refreshList();
     },
-    view(name) {
-      alert(cookie.get(name));
+    view(key) {
+      alert(cookie.get(key));
     },
-    remove(name) {
-      cookie.remove(name);
+    remove(key) {
+      cookie.remove(key);
       this.refreshList();
     },
     refreshList() {
       this.cookies = cookie.getAll();
     },
     clearForm() {
-      this.form.name = null;
+      this.form.key = null;
       this.form.value = null;
     }
   }
